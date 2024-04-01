9c182b15d4f0bcfc146c1d789151aac06136fcdc
joao nishimoto <jvonishimoto@gmail.com>
2024-04-01 15:07:43 -0300

    change token

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,8 @@ import { redirect } from "next/navigation";
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  /* const token = request.cookies.get('__Secure-next-auth.session-token')?.value */
-  const token = request.cookies.get('next-auth.session-token')?.value
+  const token = request.cookies.get('__Secure-next-auth.session-token')?.value
+  /* const token = request.cookies.get('next-auth.session-token')?.value */
   const signInURL = new URL('/auth/login', request.url)
   const mainURL = new URL('/', request.url)
 
