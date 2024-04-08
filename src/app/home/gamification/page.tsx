687b7aa54ac859e661b0615745b8b2d5d114fe95
joao nishimoto <jvonishimoto@gmail.com>
2024-04-08 16:25:14 -0300

    gamification

--- src/app/home/gamification/page.tsx
+++ src/app/home/gamification/page.tsx
@@ -17,29 +17,67 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { LightbulbIcon } from "lucide-react";
+import { CircleCheckIcon, GiftIcon, LightbulbIcon, MedalIcon } from "lucide-react";
 
 export default function Page() {
-  const [points, setPoints] = useState(0)
+  const [points, setPoints] = useState(1250)
 
   useEffect(() => {
   }, [points])
 
-  const data = [
+  const tasks = [
     {
-      name: 'Fazer tal coisa',
-      points: 800
+      name: 'Task 01',
+      points: 1800
     },
     {
-      name: 'Fazer tal coisa',
+      name: 'Task 02',
       points: 300
     },
     {
-      name: 'Fazer tal coisa',
+      name: 'Task 03',
       points: 100
     },
     {
-      name: 'Fazer tal coisa',
+      name: 'Task 04',
+      points: 200
+    }
+  ]
+
+  const rewards = [
+    {
+      name: 'Reward 01',
+      points: 800
+    },
+    {
+      name: 'Reward 02',
+      points: 2300
+    },
+    {
+      name: 'Reward 03',
+      points: 100
+    },
+    {
+      name: 'Reward 04',
+      points: 200
+    }
+  ]
+
+  const ranking = [
+    {
+      name: 'João Oliveira',
+      points: 800
+    },
+    {
+      name: 'Gustavo Miranda',
+      points: 2300
+    },
+    {
+      name: 'Gabriel Cunha',
+      points: 100
+    },
+    {
+      name: 'Camila Moura',
       points: 200
     }
   ]
@@ -50,20 +88,22 @@ export default function Page() {
         <h1 className="font-semibold text-teal-400  text-xl">
           Gustavo Miranda
         </h1>
-        <Badge className="bg-teal-400 hover:bg-teal-400 text-white font-bold py-1">
+        <Badge className="bg-teal-400 hover:bg-teal-400 text-white text-lg font-medium py-1">
           {points} <LightbulbIcon size={20} className="ml-1 text-white text-md"/>
         </Badge>
       </div>
       <div className="bg-white w-full p-4 h-16 flex items-center justify-between border border-zinc-100 rounded shadow-sm">
       <Dialog>
         <DialogTrigger asChild>
-          <Button variant="outline">Request Lúmens</Button>
+          <Button className="bg-teal-400 hover:bg-teal-500 font-semibold">
+            Request Lúmens
+          </Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>Request Lúmens 💡</DialogTitle>
+            <DialogTitle>Request Lúmens</DialogTitle>
             <DialogDescription>
-              Write a brief description below with the reasons why you receive the points. 
+              Write a brief description below with the reasons why you should receive the points. 
             </DialogDescription>
           </DialogHeader>
           <div className="grid gap-4">
@@ -79,16 +119,65 @@ export default function Page() {
             </div>
           </div>
           <DialogFooter>
-            <Button type="submit">Send Request</Button>
+            <Button className="bg-teal-500 hover:bg-teal-600 transition-all duration-300">
+              Send Request
+            </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
       </div>
       <div className="bg-white w-full px-4 py-2 flex flex-col border border-zinc-100 rounded shadow-sm">
-        {data.map(
+      <div className="inline mb-1 pb-2 w-full border-b">
+          <h1 className="inline text-2xl font-semibold text-teal-700">
+            <CircleCheckIcon className="inline mr-3"/> 
+            Tasks
+          </h1>
+        </div>
+        {tasks.map(
           (item, index) => (
             <li key={index} className="flex items-center justify-between py-2">
-              <span className="text-lg font-medium text-teal-600">
+              <span className="text-lg font-medium text-teal-500">
+                {item.name}
+              </span>
+              <Badge variant={"outline"} className="text-teal-400 text-base font-bold py-1">
+                {'+ ' + item.points} <LightbulbIcon size={20} className="ml-1 text-teal-400 text-md" />
+              </Badge>
+            </li>
+          ))}
+      </div>
+
+      <div className="bg-white w-full px-4 py-2 flex flex-col border border-zinc-100 rounded shadow-sm">
+      <div className="inline mb-1 pb-2 w-full border-b">
+          <h1 className="inline text-2xl font-semibold text-teal-700">
+            <GiftIcon className="inline mr-3"/> 
+            Rewards
+          </h1>
+        </div>
+        {rewards.map(
+          (item, index) => (
+            <li key={index} className="flex items-center justify-between py-2">
+              <span className="text-lg font-medium text-teal-500">
+                {item.name}
+              </span>
+              <Badge variant={"outline"} className="text-teal-400 text-base font-bold py-1">
+                {'- ' + item.points} <LightbulbIcon size={20} className="ml-1 text-teal-400 text-md" />
+              </Badge>
+            </li>
+          ))}
+      </div>
+
+      <div className="bg-white w-full px-4 py-2 flex flex-col border border-zinc-100 rounded shadow-sm">
+        <div className="inline mb-1 pb-2 w-full border-b">
+          <h1 className="inline text-2xl font-semibold text-teal-700">
+            <MedalIcon className="inline mr-3"/> 
+            Leaderboard
+          </h1>
+        </div>
+        {ranking
+          .sort((a, b) => b.points - a.points)
+          .map((item, index) => (
+            <li key={index} className="flex items-center justify-between py-2">
+              <span className="text-lg font-medium text-teal-500">
                 {item.name}
               </span>
               <Badge variant={"outline"} className="text-teal-400 text-base font-bold py-1">
