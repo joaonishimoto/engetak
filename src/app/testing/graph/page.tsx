a7f37bc0b032099761b1ea964ae05c9d1fb259d8
joao nishimoto <jvonishimoto@gmail.com>
2024-04-17 16:37:41 -0300

    page

--- src/app/testing/graph/page.tsx
+++ src/app/testing/graph/page.tsx
@@ -1,12 +1,12 @@
 /* import { HeatMapDemo } from "@/components/calendar/heatmap";
 import { Apointment } from "@/components/timesheet/apointment";
 
-
+ */
 export default function Page() {
   return (
     <div className="h-screen flex flex-col items-center justify-center bg-white space-y-10">
-      <Apointment />
-      <HeatMapDemo />
+{/*       <Apointment />
+      <HeatMapDemo /> */}
     </div>
   )
-} */
+}
