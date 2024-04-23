934e6eec2c6926af48e2227bed06d4b5ad960c77
joaonishimoto <jvonishimoto@gmail.com>
2024-04-23 15:37:48 -0300

    gamification

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,17 +3,17 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
     extend: {
       colors: {
         app: {
-          "050": "#f0fdfa",
+          "50": "#f0fdfa",
           "100": "#ccfbf1",
           "200": "#99f6e4",
           "300": "#5eead4",
@@ -23,21 +23,12 @@ const config = {
           "700": "#0f766e",
           "800": "#115e59",
           "900": "#134e4a",
-          "950": "#042f2e",          
-          "primary": "#14b8a6", // 500          
-          "secondary": "#99f6e4", // 200          
-          "active": "#2dd4bf", // 400   
-          "hover": "#ccfbf1", // 200           
-          "light": "#ccfbf1", // 100           
-          "dark": "#042f2e", // 950           
-          "contrast": "#fbbf24", // amber 400    
-        }
+          "950": "#042f2e",
+        },
       },
-    }
+    },
   },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
+  plugins: [require("tailwind-scrollbar")],
 } satisfies Config
 
 export default config
