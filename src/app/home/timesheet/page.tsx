99ac40f0d6500729fed7bace5ded230aeb93a5ef
joaonishimoto <jvonishimoto@gmail.com>
2024-04-24 16:46:35 -0300

    commit

--- src/app/home/timesheet/page.tsx
+++ src/app/home/timesheet/page.tsx
@@ -1,18 +1,68 @@
 "use client"
 
 import { HeatMapDemo } from "@/components/calendar/heatmap"
-import { CalendarCheck, CalendarIcon, Check } from "lucide-react"
+import { CalendarCheck, CalendarIcon } from "lucide-react"
 import { Calendar } from "@/components/ui/calendar"
-import { useState } from "react"
+import { useState, useEffect, ChangeEvent } from "react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { BiPaperPlane, BiSolidPaperPlane } from "react-icons/bi"
+import { BiSolidPaperPlane } from "react-icons/bi"
+
+interface Appointment {
+  os: string
+  item: string
+  ref: string
+  hours: string
+}
+
+const data: Appointment[] = []
 
 export default function Page() {
   const [date, setDate] = useState<Date | undefined>(new Date())
+  const [appointments, setAppointments] = useState<Appointment[]>(data)
+  const [newAppointment, setNewAppointment] = useState<Appointment>({
+    os: "",
+    item: "",
+    ref: "",
+    hours: "",
+  })
+
+  useEffect(() => {
+    setNewAppointment(data[0] || { os: "", item: "", ref: "", hours: "" })
+  }, []) // O array vazio [] garante que o useEffect só é executado uma vez, sem dependências
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setNewAppointment((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
+  const handleAddAppointment = () => {
+    setAppointments((prev) => [...prev, newAppointment])
+    setNewAppointment({
+      os: "",
+      item: "",
+      ref: "",
+      hours: "",
+    })
+  }
+
+  const calculateTotalHours = (): string => {
+    let totalHours = 0
+    appointments.forEach((appointment) => {
+      const timeParts = appointment.hours.split(":")
+      const hours = parseInt(timeParts[0], 10)
+      const minutes = parseInt(timeParts[1], 10)
+      totalHours += hours + minutes / 60
+    })
+    return totalHours.toFixed(2)
+  }
 
   return (
     <div className="p-5 min-h-screen flex flex-col space-y-5 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-5">
+      {/* Visualização de Calendário */}
       <div className="bg-white w-full h-min pb-2 flex flex-col border border-zinc-100 rounded-lg shadow-sm">
         <div className="inline mb-1 pb-2 pt-3 w-full bg-teal-300 rounded-t-lg">
           <h1 className="inline uppercase px-4 text-md font-bold text-white">
@@ -20,11 +70,12 @@ export default function Page() {
             Visualization Area
           </h1>
         </div>
+        {/* Renderização dos dados */}
         <div className="pt-4 px-4 flex flex-col">
           <div className="flex items-center justify-center">
             <HeatMapDemo />
           </div>
-          {/* Appointment */}
+          {/* Lista de Compromissos */}
           <div className="p-2">
             <div className="space-y-3">
               <div className="grid grid-cols-5 border-b pb-1 px-3">
@@ -44,61 +95,56 @@ export default function Page() {
                   ACTIONS
                 </h1>
               </div>
-              <div className="space-y-3">
-                <div className="grid grid-cols-5 px-3">
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    A307
-                  </h1>
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    01
-                  </h1>
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    2D
-                  </h1>
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    04:30
-                  </h1>
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    ...
-                  </h1>
+              {/* Mapeando os compromissos */}
+              {appointments.length == 0 ? (
+                <div className="flex items-center justify-center w-full text-md font-semibold text-zinc-500">
+                  No Results.
                 </div>
-                <div className="grid grid-cols-5 px-3">
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    A307
-                  </h1>
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    01
-                  </h1>
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    2D
-                  </h1>
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    04:30
-                  </h1>
-                  <h1 className="text-md text-zinc-500 font-semibold text-center">
-                    ...
-                  </h1>
-                </div>
-              </div>
+              ) : (
+                appointments.map((appointment, index) => (
+                  <div className="grid grid-cols-5 px-3" key={index}>
+                    <h1 className="text-md text-zinc-500 font-semibold text-center">
+                      {appointment.os}
+                    </h1>
+                    <h1 className="text-md text-zinc-500 font-semibold text-center">
+                      {appointment.item}
+                    </h1>
+                    <h1 className="text-md text-zinc-500 font-semibold text-center">
+                      {appointment.ref}
+                    </h1>
+                    <h1 className="text-md text-zinc-500 font-semibold text-center">
+                      {appointment.hours}
+                    </h1>
+                    <h1 className="text-md text-zinc-500 font-semibold text-center">
+                      ...
+                    </h1>
+                  </div>
+                ))
+              )}
+
+              {/* Total de Horas */}
               <div className="flex items-center justify-between border-t pt-1 px-5">
-                <h1 className="text-lg uppercase text-teal-400 font-bold ml-3">
+                <h1 className="text-lg uppercase text-teal-400 font-semibold ml-3">
                   Total
                 </h1>
-                <h1 className="text-lg text-teal-400 font-bold">08:00</h1>
+                <h1 className="text-lg text-teal-400 font-semibold">
+                  {calculateTotalHours()} hrs
+                </h1>
               </div>
             </div>
           </div>
         </div>
       </div>
+      {/* Área de Compromissos */}
       <div className="bg-white w-full h-min flex flex-col border border-zinc-100 rounded-lg shadow-sm">
-        <div className="inline mb- pb-2 pt-3 w-full bg-teal-300 rounded-t-lg">
+        <div className="inline mb-1 pb-2 pt-3 w-full bg-teal-300 rounded-t-lg">
           <h1 className="inline uppercase px-4 text-md font-bold text-white">
             <CalendarCheck className="inline mr-2 mb-1" />
             Appointment Area
           </h1>
         </div>
-        <div className="flex flex-col sm:flex-row items-center justify-center">
-          <div className="w-full p-5 border-b sm:border-none flex items-center justify-center">
+        <div className="p-5 w-full space-y-5 flex flex-col sm:grid sm:grid-cols-2 sm:gap-10 items-center justify-center">
+          <div>
             <Calendar
               mode="single"
               selected={date}
@@ -106,12 +152,16 @@ export default function Page() {
               className="rounded-md border w-min"
             />
           </div>
-          <div className="p-5 w-full space-y-5 flex flex-col items-center justify-center">
+          {/* Inputs */}
+          <div className="space-y-4">
             <div className="flex items-center justify-between w-56">
               <h1 className="text-zinc-500 text-lg font-semibold">OS</h1>
               <div className="flex flex-row gap-2">
                 <Input
                   type="number"
+                  name="os"
+                  value={newAppointment.os}
+                  onChange={handleInputChange}
                   className="w-24 h-10 text-md font-semibold text-zinc-500"
                 />
               </div>
@@ -121,6 +171,9 @@ export default function Page() {
               <div className="flex flex-row gap-2">
                 <Input
                   type="number"
+                  name="item"
+                  value={newAppointment.item}
+                  onChange={handleInputChange}
                   className="w-24 h-10 text-md font-semibold text-zinc-500"
                 />
               </div>
@@ -130,6 +183,9 @@ export default function Page() {
               <div className="flex flex-row gap-2">
                 <Input
                   type="text"
+                  name="ref"
+                  value={newAppointment.ref}
+                  onChange={handleInputChange}
                   className="w-24 h-10 text-md font-semibold text-zinc-500"
                 />
               </div>
@@ -139,13 +195,19 @@ export default function Page() {
               <div className="flex flex-row gap-2">
                 <Input
                   type="time"
-                  value={"08:00"}
+                  name="hours"
+                  value={newAppointment.hours}
+                  onChange={handleInputChange}
                   className="w-24 h-10 text-md font-semibold text-zinc-500"
                 />
               </div>
             </div>
+            {/* Botão de Envio */}
             <div className="w-56 text-right">
-              <Button className="bg-teal-400 hover:bg-teal-500 font-bold w-24">
+              <Button
+                onClick={handleAddAppointment}
+                className="bg-teal-400 hover:bg-teal-500 font-bold w-24"
+              >
                 <p className="mr-2">SEND</p>
                 <BiSolidPaperPlane />
               </Button>
