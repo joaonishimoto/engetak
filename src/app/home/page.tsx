8cc96c4b97c9584808d7cdbd91b8274956b9cad5
joaonishimoto <jvonishimoto@gmail.com>
2024-04-25 11:36:08 -0300

    m

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { HeatMapDemo } from "@/components/calendar/heatmap"
-import WorkingOnForm from "@/components/forms/workingOn"
 import { useSession } from "next-auth/react"
 import { text } from "stream/consumers"
 
@@ -10,7 +9,6 @@ export default function Page() {
   return (
     <div className="bg-teal-50 min-h-screen p-5 gap-5 space-y-5">
       <div className="bg-white w-full h-min pb-2 flex flex-col border border-zinc-100 rounded-lg shadow-sm">
-        <WorkingOnForm email={String(session?.user?.email)} />
       </div>
     </div>
   )
