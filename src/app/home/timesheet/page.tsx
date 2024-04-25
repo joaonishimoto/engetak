fdbc7738867d72ad85850a300401ffea4f078138
joaonishimoto <jvonishimoto@gmail.com>
2024-04-25 14:42:55 -0300

    m

--- src/app/home/timesheet/page.tsx
+++ src/app/home/timesheet/page.tsx
@@ -9,46 +9,85 @@ import { Button } from "@/components/ui/button"
 import { BiSolidPaperPlane } from "react-icons/bi"
 import { datas } from "@/database/teste/data"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { toast } from "@/components/ui/use-toast"
+import { randomUUID } from "crypto"
+
 interface Appointment {
+  id: string
   os: string
   item: string
   ref: string
   hours: string
 }
 
-const data: Appointment[] = []
+const FormSchema = z.object({
+  id: z.string(),
+  os: z.string().length(3, {
+    message: "OS must be exactly 3 characters.",
+  }),
+  item: z.string().length(2, {
+    message: "Item must be exactly 2 characters.",
+  }),
+  ref: z.string().length(2, {
+    message: "Ref must be exactly 2 characters.",
+  }),
+  hours: z.string().regex(/^([01]\d|2[0-3]):([0-5]\d)$/, {
+    message: "Hours must be in HH:mm format (e.g., 08:00).",
+  }),
+})
 
+const data: Appointment[] = []
 
 export default function Page() {
   const [date, setDate] = useState<Date | undefined>(new Date())
   const [appointments, setAppointments] = useState<Appointment[]>(data)
-  const [newAppointment, setNewAppointment] = useState<Appointment>({
+  
+/*   const [newAppointment, setNewAppointment] = useState<Appointment>({
+    id: "",
     os: "",
     item: "",
     ref: "",
     hours: "",
-  })
+  }) */
 
   useEffect(() => {
-    setNewAppointment(data[0] || { os: "", item: "", ref: "", hours: "" })
+    /* setNewAppointment(data[0] || { os: "", item: "", ref: "", hours: "" }) */
   }, []) // O array vazio [] garante que o useEffect só é executado uma vez, sem dependências
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setNewAppointment((prev) => ({
-      ...prev,
-      [name]: value,
-    }))
-  }
-
-  const handleAddAppointment = () => {
-    setAppointments((prev) => [...prev, newAppointment])
-    setNewAppointment({
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      id: String(data.length + 1),
       os: "",
       item: "",
       ref: "",
       hours: "",
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "Appointed hours!",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
     })
+    setAppointments((prevAppointments) => [...prevAppointments, data])
   }
 
   const calculateTotalHours = (): string => {
@@ -141,80 +180,102 @@ export default function Page() {
       <div className="bg-white w-full h-min flex flex-col border border-zinc-100 rounded-lg shadow-sm">
         <div className="inline mb-1 pb-2 pt-3 w-full bg-teal-300 rounded-t-lg">
           <h1 className="inline uppercase px-4 text-md font-bold text-white">
-            <CalendarCheck className="inline mr-2 mb-1" />
-            Appointment Area
+            <CalendarIcon className="inline mr-2 mb-1" />
+            Visualization Area
           </h1>
         </div>
-        <div className="p-5 w-full space-y-5 flex flex-col sm:grid sm:grid-cols-2 sm:gap-10 items-center justify-center">
-          <div>
+        <div className="flex flex-col p-5 items-center">
+          <div className="">
             <Calendar
+              className="rounded-md border w-min"
               mode="single"
               selected={date}
               onSelect={setDate}
-              className="rounded-md border w-min"
             />
           </div>
-          {/* Inputs */}
-          <div className="space-y-4">
-            {/* <h1 className='text-teal-400 font-semibold text-lg uppercase'>POINT</h1> */}
-            <div className="flex items-center justify-between w-56">
-              <h1 className="text-zinc-500 text-lg font-semibold">OS</h1>
-              <div className="flex flex-row gap-2">
-                <Input
-                  type="number"
-                  name="os"
-                  value={newAppointment.os}
-                  onChange={handleInputChange}
-                  className="w-24 h-10 text-md font-semibold text-zinc-500"
-                />
-              </div>
-            </div>
-            <div className="flex items-center justify-between w-56">
-              <h1 className="text-zinc-500 text-lg font-semibold">ITEM</h1>
-              <div className="flex flex-row gap-2">
-                <Input
-                  type="number"
-                  name="item"
-                  value={newAppointment.item}
-                  onChange={handleInputChange}
-                  className="w-24 h-10 text-md font-semibold text-zinc-500"
-                />
-              </div>
-            </div>
-            <div className="flex items-center justify-between w-56">
-              <h1 className="text-zinc-500 text-lg font-semibold">REF</h1>
-              <div className="flex flex-row gap-2">
-                <Input
-                  type="text"
-                  name="ref"
-                  value={newAppointment.ref}
-                  onChange={handleInputChange}
-                  className="w-24 h-10 text-md font-semibold text-zinc-500"
-                />
-              </div>
-            </div>
-            <div className="flex items-center justify-between w-56">
-              <h1 className="text-zinc-500 text-lg font-semibold">HOURS</h1>
-              <div className="flex flex-row gap-2">
-                <Input
-                  type="time"
-                  name="hours"
-                  value={newAppointment.hours}
-                  onChange={handleInputChange}
-                  className="w-24 h-10 text-md font-semibold text-zinc-500"
-                />
-              </div>
-            </div>
-            {/* Botão de Envio */}
-            <div className="w-56 text-right">
-              <Button
-                onClick={handleAddAppointment}
-                className="bg-teal-400 hover:bg-teal-500 font-bold w-24"
-              >
-                <p className="mr-2">SEND</p>
-                <BiSolidPaperPlane />
-              </Button>
-            </div>
+          <div className="flex flex-col p-5 w-72">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="w-full ">
+                <div className="gap-10 grid grid-cols-2">
+                  <div className="">
+                    <FormField
+                      control={form.control}
+                      name="os"
+                      render={({ field }) => (
+                        <div className="space-y-3">
+                          <FormItem>
+                            <FormLabel>OS</FormLabel>
+                            <FormControl>
+                              <Input placeholder="000" {...field} />
+                            </FormControl>
+                            <FormDescription></FormDescription>
+                            <FormMessage />
+                          </FormItem>
+                        </div>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="item"
+                      render={({ field }) => (
+                        <div className="space-y-3">
+                          <FormItem>
+                            <FormLabel>ITEM</FormLabel>
+                            <FormControl>
+                              <Input placeholder="00" {...field} />
+                            </FormControl>
+                            <FormDescription></FormDescription>
+                            <FormMessage />
+                          </FormItem>
+                        </div>
+                      )}
+                    />
+                  </div>
+                  <div className="">
+                    <FormField
+                      control={form.control}
+                      name="ref"
+                      render={({ field }) => (
+                        <div className="space-y-3">
+                          <FormItem>
+                            <FormLabel>REF</FormLabel>
+                            <FormControl>
+                              <Input placeholder="2D" {...field} />
+                            </FormControl>
+                            <FormDescription></FormDescription>
+                            <FormMessage />
+                          </FormItem>
+                        </div>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="hours"
+                      render={({ field }) => (
+                        <div className="space-y-3">
+                          <FormItem>
+                            <FormLabel>HOURS</FormLabel>
+                            <FormControl>
+                              <Input type="time" {...field} />
+                            </FormControl>
+                            <FormDescription></FormDescription>
+                            <FormMessage />
+                          </FormItem>
+                        </div>
+                      )}
+                    />
+                  </div>
+                </div>
+                <div className="flex items-center justify-center w-full mt-5">
+                  <Button
+                    type="submit"
+                    className="bg-teal-400 hover:bg-teal-400 font-semibold w-full"
+                  >
+                    Submit
+                  </Button>
+                </div>
+              </form>
+            </Form>
           </div>
         </div>
       </div>
